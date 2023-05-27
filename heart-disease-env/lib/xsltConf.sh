25b69206a581d196dfdf68ce4969d06fc05a301f
Nathaniel <clevernat1@gmail.com>
2023-05-27 04:04:25 -0600

    Initial Commit

---
+++ heart-disease-env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/nat/Desktop/main/machine-learning/heart-disease-project/heart-disease-env/lib"
+XSLT_LIBS="-lxslt -L/Users/nat/Desktop/main/machine-learning/heart-disease-project/heart-disease-env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/nat/Desktop/main/machine-learning/heart-disease-project/heart-disease-env/include"
+MODULE_VERSION="xslt-1.1.37"
